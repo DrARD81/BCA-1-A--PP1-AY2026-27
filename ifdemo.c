a5dba12605b973a6e88e9cdbeba42de507949881
DrARD81 <ashwin.dobariya@marwadieducation.edu.in>
2026-07-20 08:29:32 +0530

    Add files via upload

---
+++ ifdemo.c
@@ -0,0 +1,25 @@
+//Demo C Operators
+// Write a program which inputs five subject marks and displays result pass / fail (Passing is 40 marks)
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x;
+
+    printf("\n Enter your age : ");
+    scanf("%d",&x);
+
+    if(x >= 18)
+    {
+        printf("\n You are eligible for voting");
+    }
+    else
+    {
+        printf("\n You are not eligible for voting");
+    }
+    printf("\n Good bye");
+
+
+}
